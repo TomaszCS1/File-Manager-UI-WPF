5e1610aab95d7f7c039b7eb1a8bcf2b59b8a3933
Thomas Pfaffinger <thomas.pfaffinger@softronik.de>
2022-11-18 17:02:23 +0100

    weitere...

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dateien_Umbenennen.Core;
 
 namespace Dateien_Umbenennen.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        private object myVar;
 
+        public object MyProperty        //30:49
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public MainViewModel()
+        {
+
+        }
     }
 }
