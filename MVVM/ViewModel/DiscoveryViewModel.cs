822df27319dd5684248901612c6c2e7085807fef
Thomas Pfaffinger <thomas.pfaffinger@softronik.de>
2022-11-21 16:56:15 +0100

    lambda expression Problem

---
+++ MVVM/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dateien_Umbenennen.MVVM.ViewModel
+{
+    class DiscoveryViewModel
+    {
+    }
+}
--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using Dateien_Umbenennen.Core;
-
+using GalaSoft.MvvmLight.Command;
+using System.Linq.Expressions;
 
 namespace Dateien_Umbenennen.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
 
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
         public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
 
         private object _currentView;
-
+         
         public object CurrentView
         {
             get { return _currentView; }
@@ -25,7 +30,19 @@ namespace Dateien_Umbenennen.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
             CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(eingabeparameter => {
+                CurrentView = HomeVm;
+            });
+
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVm;
+            });
+
 
         }
     }
